24ae442d27e1f213978ea761d537e0bf40424a7c
Lydia Thornton <lydia.d.thornton@gmail.com>
2018-08-12 22:27:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,6 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object.key;
+  var object = Object.assign(object, {});
+  return delete clone['key'];
 }
