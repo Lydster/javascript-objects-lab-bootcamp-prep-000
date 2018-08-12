4dcf0f071c853f100c459e1d1ef2fd58dd52ff96
Lydia Thornton <lydia.d.thornton@gmail.com>
2018-08-12 22:20:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var clone = Object.assign(object, {});
+  clone = Object.assign(object, {});
   delete clone{key];
 }
 
