68fc79607961d8a88f8be138342786a3d0cc6b1f
Lydia Thornton <lydia.d.thornton@gmail.com>
2018-08-12 21:47:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var recipes = {};
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value});
 }
+
+function destructivelyUpdateObjectsWithKeyAndValue(object, key, value) {
+  return object.key = value
+}
